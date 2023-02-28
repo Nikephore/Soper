95c7274ea318e6322359d53799cfb6ff69b2ff04
Tu Nombre <you@example.com>
2023-02-28 15:48:45 +0100

    avanzando ejercicio 1a

--- p1/src/pow.c
+++ p1/src/pow.c
@@ -1,4 +1,7 @@
 #include "pow.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 #define PRIME POW_LIMIT
 #define BIG_X 435679812
@@ -8,54 +11,75 @@
 #define MAX_CYCLES 20
 #define ERROR -1
 
-long int pow_hash(long int x) {
+typedef struct
+{
+  int min;
+  int max;
+  int target;
+} search;
+
+long int pow_hash(long int x)
+{
   long int result = (x * BIG_X + BIG_Y) % PRIME;
   return result;
 }
 
-*void target_search(rango definido){
-  
-  //bucle que busque en pow hash desde s_ini a s_fin
-  //comprobar variable global
-  
+void * target_search(void *objective)
+{
+  const search *obj = objective;
+  int result;
 
-}
-
-int main(int argc, char **argv){
-
-    int n_threads;
-    int n_cycles;
-
-    //array de 0 a POW_LIMIT-1
-
-    //igualar variables a argv[]
-
-    if(n_cycles > 0){
-      for(int a = 0; a < n_cycles;  a++){
-        for(int i = 0; i < n_threads; i++){
-          //crear hilo
-
-        }
-        for(int i = 0; i < n_threads; i++){
-          //join hilos
-        }
-
-        
-      }
+  for(int i = obj->min; i <= obj->max; i++){
+    result = pow_hash(i);
+    if(result == obj->target){
+      
     }
-}
-
-/*
-main(n_t, n_c, target_inicial){
-
-  for(n_c){
-    sol = solve(nthreads, tini)
-    tini = sol
   }
-}
-*/
 
-int main(int argc, char *argv[]){
+  // bucle que busque en pow hash desde s_ini a s_fin
+  // comprobar variable global
+}
+
+int solve(int n_threads, int target)
+{
+  int solution = -1;
+  int error;
+  pthread_t threads[n_threads];
+  search objective[n_threads];
+
+  // MAX / MAX_THREADS * i,  MAX / MAX_THREADS * (i+1) -1
+
+  // 10000 / 100 = 100 * 0  --> 100
+
+  for (int i = 0; i < n_threads; i++)
+  {
+
+    objective[i].min = POW_LIMIT / MAX_THREADS * i;
+    objective[i].max = (POW_LIMIT / MAX_THREADS * (i + 1)) - 1;
+    objective[i].target = target;
+    error = pthread_create(&threads[i], NULL, target_search, objective);
+    if (error != 0)
+    {
+      fprintf(stderr, "pthread_create: %s\n", strerror(error));
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  for (int i = 0; i < n_threads; i++)
+  {
+    error = pthread_join(threads[i], NULL);
+    if (error != 0)
+    {
+      fprintf(stderr, "pthread_join: %s\n", strerror(error));
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return solution;
+}
+
+int main(int argc, char *argv[])
+{
 
   int n_cycles;
   int n_threads;
@@ -63,7 +87,8 @@ int main(int argc, char *argv[]){
   int solution;
 
   /* Control de errores num arguentos*/
-  if(argc != 4) {
+  if (argc != 4)
+  {
     printf("No se ha pasado el numero correcto de argumentos\n");
     printf("El formato correcto es:\n");
     printf("./programa <Target inicial> <Num_ciclos> <Num_hilos>");
@@ -71,61 +96,44 @@ int main(int argc, char *argv[]){
   }
 
   target_ini = atoi(argv[1]);
-  if(target_ini < 0){
+  if (target_ini < 0)
+  {
     printf("El target no puede ser inferior a 0");
     return ERROR;
   }
 
   /* Establecemos el numero de ciclos */
   n_cycles = atoi(argv[2]);
-  if(n_threads < 1){
+  if (n_cycles < 1)
+  {
     printf("El numero de ciclos no puede ser inferior a 1");
     return ERROR;
   }
 
-  if(n_threads > MAX_CYCLES){
-    printf("El numero de hilos no puede ser superior a %d", MAX_CYCLES);
+  if (n_cycles > MAX_CYCLES)
+  {
+    printf("El numero de ciclos no puede ser superior a %d", MAX_CYCLES);
     return ERROR;
   }
 
   /* Establecemos el numero de hilos */
   n_threads = atoi(argv[3]);
-  if(n_threads < 1){
+  if (n_threads < 1)
+  {
     printf("El numero de hilos no puede ser inferior a 1");
     return ERROR;
   }
 
-  if(n_threads > MAX_THREADS){
+  if (n_threads > MAX_THREADS)
+  {
     printf("El numero de hilos no puede ser superior a %d", MAX_THREADS);
     return ERROR;
   }
 
-  for(i = 0; i < n_cycles; i++){
-    printf("Estamos en el bucle %d", i+1);
+  for (int i = 0; i < n_cycles; i++)
+  {
+    printf("Estamos en el bucle %d", i + 1);
     solution = solve(n_threads, target_ini);
     target_ini = solution;
   }
-
 }
-
-int solve(int n_threads, int target){
-  int solution = -1;
-  pthread_t threads[nthreads];
-
-  for(i = 0; i < n_threads; i++){
-    
-  }
-
-
-
-  return solution
-}
-
-/*
-solve(n_t, t){
-  por cada hilo
-    create thread -> cada uno en un rango
-  por cada hilo
-    join thread
-}
-*/
